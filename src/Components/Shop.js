2fdcdb6bb9c11e00b792325cc3b9bfc41bd1dbbd
aarti-soni <aartisoni32748@gmail.com>
2024-04-06 09:08:43 +0530

    [Added] Basic Setup for demo

---
+++ src/Components/Shop.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Shop = () => {
+    return (
+        <div>
+            <>
+            <div className='container'><br/>
+                <h2>Buy Addidas shoes</h2>
+                <h6>RS. 50</h6><br/>
+
+                <button className='btn btn-primary mx-2' >-</button> Add To Cart
+                <button className='btn btn-primary mx-2' >+</button>
+            </div>
+            </>
+        </div>
+    )
+}
+
+export default Shop;
