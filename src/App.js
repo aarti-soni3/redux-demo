4e20eed333841f11c31382558d989228cd39280f
aarti-soni <aartisoni32748@gmail.com>
2024-04-10 23:26:43 +0530

    [Update] Demo with action.payload

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { increment, decrement } from './reduxToolKit/Slices/Counter/CounterSlice';
 
@@ -6,6 +7,15 @@ function App() {
 
   const count = useSelector((state) => state.counter);
   const dispatch = useDispatch();
+  const [number,setNumber] = useState(0);
+
+  const handleIncrement = () => {
+    dispatch(increment(Number(number)))
+  }
+
+  const handleDecrement = () => {
+    dispatch(decrement(Number(number)))
+  }
 
   return (
     <div className="App">
@@ -13,8 +23,9 @@ function App() {
         {console.log(count)}
         <h1>Count : {count === undefined ? "undefined" : count}</h1>
       </div><br />
-      <button onClick={() => dispatch(increment())}>inc</button>
-      <button onClick={() => dispatch(decrement())}>dec</button>
+      <input type="number" value={number} onChange={(e) => setNumber(e.target.value)} />
+      <button onClick={handleIncrement}>inc</button>
+      <button onClick={handleDecrement}>dec</button>
       {/*
         The problem is that you're invoking the function immediately and then what's left is the return value,
         which might not be a function!
--- src/reduxToolKit/Slices/Counter/CounterSlice.js
+++ src/reduxToolKit/Slices/Counter/CounterSlice.js
@@ -4,8 +4,8 @@ export const counterSlice = createSlice({
     initialState: 0,
     name: 'counter',
     reducers: {
-        increment: (state) => state + 1,
-        decrement: (state) => state - 1,
+        increment: (state,action) => state + action.payload,
+        decrement: (state,action) => state - action.payload,
     }
 });
 
