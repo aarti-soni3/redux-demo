f1338ad5701fcc0fffd8cde18f8ffd766be8e9d0
aarti-soni <aartisoni32748@gmail.com>
2024-04-10 22:52:52 +0530

    [Added] React Toolkit Demo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import './App.css';
-import Navbar from './Components/Navbar';
-import Shop from './Components/Shop';
+import { useSelector, useDispatch } from 'react-redux';
+import { increment, decrement } from './reduxToolKit/Slices/Counter/CounterSlice';
 
 function App() {
+
+  const count = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
+
   return (
     <div className="App">
-      <Navbar/>
-      <Shop/>
+      <div>
+        {console.log(count)}
+        <h1>Count : {count === undefined ? "undefined" : count}</h1>
+      </div><br />
+      <button onClick={() => dispatch(increment())}>inc</button>
+      <button onClick={() => dispatch(decrement())}>dec</button>
+      {/*
+        The problem is that you're invoking the function immediately and then what's left is the return value,
+        which might not be a function!
+
+        What you can do instead is wrap that function call inside an arrow function to solve your problem. 
+        It'll call the inner function once you onClick:
+       */}
     </div>
   );
 }
---
+++ src/reduxToolKit/Store.js
@@ -0,0 +1,9 @@
+import {configureStore} from '@reduxjs/toolkit';
+import { counterSlice } from './Slices/Counter/CounterSlice';
+
+export const store = configureStore({
+    reducer :{
+        counter : counterSlice.reducer,
+    },
+});
+
